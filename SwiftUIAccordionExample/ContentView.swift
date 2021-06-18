49718fa6ac24341ad5ddff98a807936a1579219d
Xinjiang Shao <xinjiang.shao@hey.om>
2021-06-18 17:42:33 -0500

    Initial Commit

---
+++ SwiftUIAccordionExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAccordionExample
+//
+//  Created by Xinjiang Shao on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAccordionExample/SwiftUIAccordionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccordionExampleApp.swift
+//  SwiftUIAccordionExample
+//
+//  Created by Xinjiang Shao on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccordionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
